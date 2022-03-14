a8ac663aac28c13879398989be54f187a2ca9361
Stefan Popa <popashtefan10@gmail.com>
2022-03-14 17:30:14 +0200

    Primul commit functional

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
-#include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
-int main() {
-    std::cout << "Hello, world!\n";
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        sf::CircleShape shape(50.f);
+
+        // set the shape color to green
+        shape.setFillColor(sf::Color(100, 250, 50));
+
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event{};
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
