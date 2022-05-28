5dbcf488ef0fcca243306eaa173498c5da027f04
Stefan Popa <popashtefan10@gmail.com>
2022-05-28 18:39:45 +0300

    Clasa Level

---
+++ headers/Level.h
@@ -0,0 +1,28 @@
+//
+// Created by popas on 28.05.2022.
+//
+
+#ifndef OOP_LEVEL_H
+#define OOP_LEVEL_H
+
+#include <string>
+#include "SFML/Graphics.hpp"
+
+class Level {
+
+private:
+    std::string mapConfigFilename;
+    sf::Vector2u startPosition;
+    sf::Vector2u endPosition;
+
+public:
+    Level();
+    Level(std::string, sf::Vector2u, sf::Vector2u);
+
+    std::string getMapConfigFilename();
+    sf::Vector2u getStartPosition();
+    sf::Vector2u getEndPosition();
+};
+
+
+#endif //OOP_LEVEL_H
---
+++ sources/Level.cpp
@@ -0,0 +1,24 @@
+//
+// Created by popas on 28.05.2022.
+//
+
+#include "../headers/Level.h"
+
+#include <utility>
+
+Level::Level() : startPosition({0, 0}), endPosition({0, 0}) {}
+
+Level::Level(std::string mapConfigFilename, sf::Vector2u startPosition, sf::Vector2u endPosition) :
+    mapConfigFilename(std::move(mapConfigFilename)), startPosition(startPosition), endPosition(endPosition) {}
+
+std::string Level::getMapConfigFilename() {
+    return mapConfigFilename;
+}
+
+sf::Vector2u Level::getStartPosition() {
+    return startPosition;
+}
+
+sf::Vector2u Level::getEndPosition() {
+    return endPosition;
+}
